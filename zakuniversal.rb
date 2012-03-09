9b489e5204e9041f1e40814cdf342ca6a1758fdd
zakuni <kunio038@gmail.com>
2012-03-09 21:00:55 +0900

    translating zakuni's tweets to English

--- zakuniversal.rb
+++ zakuniversal.rb
@@ -1,8 +1,11 @@
 require 'bing_translator'
 require 'yaml'
+require 'twitter'
 
 config = YAML.load_file('./config.yaml')
 BT_APP_ID = config['BT_APP_ID']
 
+tweets = Twitter.user_timeline("zakuni").map do |tweet| tweet.text end
+
 bt = BingTranslator.new(BT_APP_ID)
-puts bt.translate('hello', params = {:to => 'ja'})
+tweets.each do |tweet| puts bt.translate(tweet, params = {:to => 'en'}) end
