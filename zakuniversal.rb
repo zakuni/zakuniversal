8790f5e00c8bc3037dea0b46c0d269cfeca5f71b
zakuni <kunio038@gmail.com>
2012-03-10 20:44:52 +0900

    storing translated tweets to new collections, renamed related things

--- zakuniversal.rb
+++ zakuniversal.rb
@@ -3,38 +3,49 @@ require 'yaml'
 require 'twitter'
 require 'mongo'
 
-config = YAML.load_file('./config.yaml')
-BT_APP_ID = config['BT_APP_ID']
+@conf = YAML.load_file('./config.yaml')
+BT_APP_ID = @conf['BT_APP_ID']
 
-@conn = Mongo::Connection.new(config['mongo_server'], config['mongo_port'])
-@db = @conn[config['mongo_db']]
-@coll = @db[config['mongo_coll']]
+@conn = Mongo::Connection.new(@conf['mongo_server'], @conf['mongo_port'])
+@db = @conn[@conf['mongo_db']]
+@baseColl = @db[@conf['mongo_coll']]
+@translatedColl = @db['translatedtweets']
 
 @bt = BingTranslator.new(BT_APP_ID)
-langs = @bt.supported_language_codes
-@lang = langs[rand(langs.length)]
+@langs = @bt.supported_language_codes
 
-tweets = Twitter.user_timeline("zakuni")
+Twitter.configure do |config|
+  config.consumer_key = @conf['consumer_key'] 
+  config.consumer_secret = @conf['consumer_secret']
+  config.oauth_token = @conf['oauth_token']
+  config.oauth_token_secret = @conf['oauth_token_secret']
+end
 
 def store(tweet)
-	if @coll.find('tweet_id' => tweet.id).count == 0 then
-		@coll.insert({'tweet_id' => tweet.id, 'text' => tweet.text})
+	if @baseColl.find('tweet_id' => tweet.id).count == 0 then
+		@baseColl.insert({'tweet_id' => tweet.id, 'text' => tweet.text})
+		puts "stored"
 	else
 		puts "already stored"
 	end
 end
 
 def translate(text)
-	@bt.translate(text, parmas = {:to => @lang})
+	lang = @langs[rand(@langs.length)]
+	{"lang" => lang, "text" => @bt.translate(text, parmas = {:to => lang})}
 end
 
-tweets.each do |tweet| 
+base_tweets = Twitter.user_timeline("zakuni")
+base_tweets.each do |tweet| 
 	store(tweet)
 end
 
-@coll.find.each do |tweet|
+# tweeted = Twitter.update("nemui")
+# @translatedColl.insert({'tweet_id' => tweeted.id})
+
+@baseColl.find.each do |tweet|
 	text = tweet["text"]
-	puts text 
-	puts translate(text)
+	translated = translate(text)
+	puts "#{text}\n[#{translated['lang']}]#{translated['text']}"
 end
 
