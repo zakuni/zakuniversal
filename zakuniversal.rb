fdcaafc76333f050101998613bad481910909885
zakuni <kunio038@gmail.com>
2012-03-11 00:33:06 +0900

    getting a random data from basetweets_collection

--- zakuniversal.rb
+++ zakuniversal.rb
@@ -8,11 +8,13 @@ BT_APP_ID = @conf['BT_APP_ID']
 
 @conn = Mongo::Connection.new(@conf['mongo_server'], @conf['mongo_port'])
 @db = @conn[@conf['mongo_db']]
-@baseColl = @db[@conf['mongo_coll']]
-@translatedColl = @db['translatedtweets']
+@base_coll = @db[@conf['mongo_coll']]
+@translated_coll = @db['translatedtweets']
 
 @bt = BingTranslator.new(BT_APP_ID)
 @langs = @bt.supported_language_codes
+
+puts @base_coll.find().limit(-1).skip(rand(@base_coll.count)).next
 
 Twitter.configure do |config|
   config.consumer_key = @conf['consumer_key'] 
@@ -22,8 +24,8 @@ Twitter.configure do |config|
 end
 
 def store(tweet)
-	if @baseColl.find('tweet_id' => tweet.id).count == 0 then
-		@baseColl.insert({'tweet_id' => tweet.id, 'text' => tweet.text})
+	if @base_coll.find('tweet_id' => tweet.id).count == 0 then
+		@base_coll.insert({'tweet_id' => tweet.id, 'text' => tweet.text, 'random' => rand})
 		puts "stored"
 	else
 		puts "already stored"
@@ -41,11 +43,10 @@ base_tweets.each do |tweet|
 end
 
 # tweeted = Twitter.update("nemui")
-# @translatedColl.insert({'tweet_id' => tweeted.id})
+# @translated_coll.insert({'tweet_id' => tweeted.id})
 
-@baseColl.find.each do |tweet|
-	text = tweet["text"]
+@base_coll.find.each do |base_tweet|
+	text = base_tweet["text"]
 	translated = translate(text)
 	puts "#{text}\n[#{translated['lang']}]#{translated['text']}"
 end
-
