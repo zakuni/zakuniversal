94c589dd1b8d197539355c34757def92fd66b939
zakuni <kunio038@gmail.com>
2012-03-10 15:21:47 +0900

    store tweets

--- zakuniversal.rb
+++ zakuniversal.rb
@@ -6,19 +6,28 @@ require 'mongo'
 config = YAML.load_file('./config.yaml')
 BT_APP_ID = config['BT_APP_ID']
 
-tweets = Twitter.user_timeline("zakuni").map do |tweet| 
-	tweet.text 
-end
-
 @conn = Mongo::Connection.new(config['mongo_server'], config['mongo_port'])
-@db = @conn['shelf']
-@coll = @db['books']
-
-@coll.find.each { |doc| puts doc.inspect }
+@db = @conn[config['mongo_db']]
+@coll = @db[config['mongo_coll']]
 
 bt = BingTranslator.new(BT_APP_ID)
 lang = bt.supported_language_codes
 
+tweets = Twitter.user_timeline("zakuni")
+
+def store(tweet)
+	if @coll.find('tweet_id' => tweet.id).count == 0 then
+		@coll.insert({'tweet_id' => tweet.id, 'text' => tweet.text})
+	else
+		puts "already stored"
+	end
+end
+
 tweets.each do |tweet| 
-	puts bt.translate(tweet, params = {:to => lang[rand(lang.length)]}) 
+	store(tweet)
+	puts bt.translate(tweet.text, params = {:to => lang[rand(lang.length)]}) 
+end
+
+@coll.find.each do |doc|
+	puts doc.inspect 
 end
